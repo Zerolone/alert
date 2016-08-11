71191558324f0d29edec9c0c915c3fa2b2895c49
ydq <ydq@users.noreply.github.com>
2016-08-11 14:05:34 +0800

    新增功能

--- alert.js
+++ alert.js
@@ -16,21 +16,21 @@
 			}
 		},
 		confirm:function(){
-			var args=arguments;
+			var args=arguments,d;
 			if(args.length){
-				var d =$('<div class="alert_overlay"></div><div class="alert_msg"><div class="alert_content">'+args[0]+'</div><div class="alert_buttons"><button class="alert_btn alert_btn_ok">确定</button><button class="alert_btn alert_btn_cancel">取消</button></div></div>'),
-				fn=args[1],
-				flag=1,
-				_click = function(e){
-					typeof fn=='function'?(fn.call(d,e.data.r)!=!1&&d.remove()):d.remove();
-				};
-				$._isalert&&d.find('.alert_btn_cancel').hide();
-				d.on('contextmenu',!1)
-				.on('click','.alert_btn_ok',{r:!0},_click)
-				.on('click','.alert_btn_cancel',{r:!1},_click)
-				.appendTo('body').find('.alert_btn_ok').focus();
+				var fn=args[1],_click = function(e){typeof fn=='function'?(fn.call(d,e.data.r)!=!1&&d.close()):d.close();};
+				d = $('<div class="alert_overlay"><div class="alert_msg"><div class="alert_content">'+args[0]+'</div><div class="alert_buttons"><button class="alert_btn alert_btn_ok">确定</button><button class="alert_btn alert_btn_cancel">取消</button></div></div></div>').on('contextmenu',!1).on('click','.alert_btn_ok',{r:!0},_click).on('click','.alert_btn_cancel',{r:!1},_click)
+				$._isalert&&d.find('.alert_btn_cancel').css('display','none');
+				d.ok =  function(t){d.find('.alert_btn_ok').text(t||'确定');return d}
+				d.cancel=function(t){d.find('.alert_btn_cancel').text(t||'取消');return d}
+				d.padding=function(p){d.find('.alert_content').css('padding',p||0);return d}
+				d.width=function(w){w=!w||w<200?200:w,d.find('.alert_msg').css({width:w,marginLeft:-w/2});return d}
+				d.content=function(t){t&&d.find('.alert_content').html(t);return d}
+				d.close=d.remove;
+				d.appendTo('body').find('.alert_btn_ok').focus()
 			}
 			$._isalert=0;
+			return d;
 		},
 		tips:function(){
 			var args=arguments;
